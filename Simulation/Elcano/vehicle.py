cda2b84b92c772ca1802be2774ae7e51a671b22b
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2020-02-17 02:05:50 -0800

    Big push to object oriented

--- Simulation/Elcano/vehicle.py
+++ Simulation/Elcano/vehicle.py
@@ -1,27 +1,24 @@
 import serial
 import Carla
 
-
-
-
+#Vehicle class, contans
 class Vehicle:
+
+    #Constructor for vehicle class
     def __init__(self):
 
-        #Carla vehicle options
-        vehiclesOpt = ["trike"]
-        vehicle = None  # actor ID of the spawned vehicle
-        sensorsOptDisp = ["nmeaGPS","rotSensor","numGPS"]
-        sensorsOpt = ["nmeagps","rotsensor","numgps"]
-        sensors = []
+        #Variables related to simulator
+        self.sensors = []
+        self.speed = 0
+        self.steeringangle = 0
 
+        #Variables for vehicle
+        #Pulled these from old code, not sure use of them.
+        #self.stopListen
+        #self.pulse
 
-
-
-
-    def initialize(self, host = 'localhost', port = 2000):
-
-        #Set COM port for router communication
-        #self.ser = serial.Serial('COM4',baudrate = 115200,timeout=1)
+    #Initialize connection to simulator and spawn vehicle
+    def connectToSim(self, host = 'localhost', port = 2000, headless = False):
 
         #Attempt to connect to the Carla server, timeout after 5 seconds
         self.client = Carla.Client(host, port)
@@ -31,20 +28,91 @@ class Vehicle:
         self.world = self.client.get_world()
 
         #Load Carlas blueprint library
-        blueprint_library = self.world.get_blueprint_library()
+        self.blueprint_library = self.world.get_blueprint_library()
 
         #Grab all the car blueprints from library
-        vehicle = blueprint_library.find('vehicle.bmw.isetta')
+        vehicle = self.blueprint_library.find('vehicle.bmw.isetta')
 
         #Spawns trike at spawn point
         spawn = Carla.Transform(Carla.Location(x=15, y=20, z=5), Carla.Rotation(yaw=180))
         self.actor = self.world.spawn_actor(vehicle, spawn)
 
-        ### SET SIM VIEWPOINT TO VEHICLE ####
+        #Move server viewpoint to trike, will move this out of function with
+        #different options in UI for camera setup.
         self.world.get_spectator().set_transform(self.actor.get_transform())
 
-    
+        if(headless):
+            #Enable headless mode
+            settings = self.world.get_settings()
+            settings.no_rendering_mode = True
+            world.apply_settings(settings)
 
+        self.spawnSensors()
+
+    #Initialize connection to routerboard
+    def connectToRouter(self, port = 'COM4'):
+
+        #Set COM port for router communication
+        self.routerboard = serial.Serial(port, baudrate = 115200, timeout=5)
+
+        #Notify Due that system is starting
+        self.routerboard.write('f'.encode('utf-8')) 
+
+    #For spawning all sensors onto vehicle, add sensors here.
+    def spawnSensors(self):
+
+        #Create and add all desired sensors
+        self.sensors.append(NMEA(self.world, self.actor))
+        self.sensors.append(IMU(self.world, self.actor))
+
+    #For destroying the vehicle in simulator
+    def destroy(self):
+        for sensor in sensors:
+            sensor.destroy()
+        actor.destroy()
+
+    #Demo code, in the works.
+    def demo(self):
+
+        while True:
+            # Wait for ready queue from due
+            if self.routerboard.in_waiting:
+                
+                ''' 
+                ADD SERIAL READS FOR ACTUATION HERE 
+                
+                Currently just clears an arbitrary char in buffer sent from
+                router Due
+                '''
+
+                ## Receive in order: throttle, steer, brake
+                t = float(self.routerboard.readline().decode('ASCII'))
+                s = float(self.routerboard.readline().decode('ASCII'))
+                b = float(self.routerboard.readline().decode('ASCII'))
+                
+                actor.apply_control(Carla.VehicleControl(throttle=t,steer=s,brake=b))
+
+                ''' 
+                Finish processing actuation commands here
+                
+                Here's how data is sent from Due:
+                - Throttle : float (-1 to 1)
+                - Steering : float (-1 to 1)
+                - Brakes   : float (0 for off 0.3 for on) <- because current implementation of brake
+                    is siimply on/off.  Feel free to change on value of 0.3.
+                '''
+                
+                ### ACCESS/SEND LAT/LONG FROM LAST TICK ###
+                ### Can use for location if disabling GPS Sensor
+
+                #geo = world.get_map().transform_to_geolocation(logger.actorId.get_location())
+                #msg = geo.__str__() + '@'
+                #logger.setNmea(msg)
+                ###########
+                
+                
+        
+    #Copied this function over will likely end up erased
     def getSpeed(self, logger):
 
         actor = self.actor
@@ -64,3 +132,26 @@ class Vehicle:
         
         logger.setCyclometer(asString)
         #dueNative.write(asString.encode('utf-8'))
+
+############ Will move these sensors into their own import ############
+#Each sensor will likely be sublasses of greater Sensor class
+#Will be able to determine values from carla and send them to router
+
+class NMEA:
+    def __init__(self, world, actor):
+
+        # Attach nmeaSensor to trike (speed directly taken from trike actor)
+        blueprint = world.get_blueprint_library().find('sensor.other.gnss')
+        transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
+        self.sensor = world.spawn_actor(blueprint,transform,actor)
+        
+class IMU:
+    def __init__(self, world, actor):
+
+        #Get sensor and spawn it onto the vehicle
+        #Failed to find sensor in test, will look into this
+        #blueprint = world.get_blueprint_library().find('sensor.other.imu')
+        #transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
+        #self.sensor = world.spawn_actor(blueprint, transform, actor)
+
+        self.name = IMU
--- Simulation/simulator.py
+++ Simulation/simulator.py
@@ -50,157 +50,19 @@ import Elcano
 print("Welcome to the Elcano Project Simulation")
 input("Press enter when prepared to connect to server")
 
-#Create the vehicle and spawn in server
+#Create the vehicle and connect to simulator and routerboard
 trike = Elcano.Vehicle()
-trike.initialize()
-
-#Data Logger logging functions, not 100% sure how this operates
-logging.basicConfig(format='%(levelname)s: %(message)s', level=logging.INFO)
-
-#Attempt to connect to the Carla server, timeout after 5 seconds
-client = Carla.Client('localhost', 2000)
-client.set_timeout(5.0)
-
-#Get the world from the server
-world = client.get_world()
-
-#Enable headless mode
-#settings = world.get_settings()
-#settings.no_rendering_mode = True
-#world.apply_settings(settings)
-
-#Load Carlas blueprint library
-blueprint_library = world.get_blueprint_library()
-
-#Grab all the car blueprints from library
-vehicle = blueprint_library.find('vehicle.bmw.isetta')
-
-#Gather possible spawn points
-spawn_points = world.get_map().get_spawn_points()
-number_of_spawn_points = len(spawn_points)
-
-#Set COM port for router communication
-ser = serial.Serial('COM4',baudrate = 115200,timeout=1)
-
-#CLI when starting script
-print("Successful connection")     
-
-#Spawns trike at spawn point
-spawn = Carla.Transform(Carla.Location(x=15, y=20, z=5), Carla.Rotation(yaw=180))
-actor = world.spawn_actor(vehicle, spawn)
-
-#Logging Data from the actor
-logger = DataLogger(actor)
+trike.connectToSim()
+#trike.connectToRouter()
 
 
-# Attach nmeaSensor to trike (speed directly taken from trike actor)
-blueprint = world.get_blueprint_library().find('sensor.other.gnss')
-transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
-nmeaSensor = world.spawn_actor(blueprint,transform,actor)
-sensors.append(nmeaSensor)
-
-# ---------------Attach IMU sensor to trike (taken from trike actor--------------------
-blueprintIMU = world.get_blueprint_library().find('sensor.other.imu')
-# provide the position of the IMU sensor relative to the vehicle
-transformIMU = carla.Transform(carla.Location(x=0.8, z=1.7))
-# makes Carla world spawn sensor and attach it to the vehicle actor
-sensorIMU = world.spawn_actor(blueprintIMU, transformIMU, actor)
-sensors.append(sensorIMU)
-# -------------------------------------------------------------------------------------
-
-#speedSensor = world.spawn_actor(blueprint,transform,actor)
-
-### SET SIM VIEWPOINT TO VEHICLE ####
-world.get_spectator().set_transform(actor.get_transform())
 
 try:
-    time.sleep(5)
-    ser.write('f'.encode('utf-8'))  #Notify Due that system is starting
     
-    ##### FOR GPS SENSOR USAGE ####
-    logger.setListen()
-    logger.setNmea('Test@')  # Fill private member to avoid error
-    #nmeaSensor.listen(lambda data: nmeaGPS.consoleout(data,logger))
-    ##########################
-    
-    while True:
-        # Wait for ready queue from due
-        if ser.in_waiting:
-            ### FOR GPS SENSOR USAGE (Set the stop variable to short the listener func) ###
-            logger.setStopListen()
-            #nmeaSensor.stop()
-            ###########
-            
-            ''' 
-            ADD SERIAL READS FOR ACTUATION HERE 
-            
-            Currently just clears an arbitrary char in buffer sent from
-            router Due
-            '''
-
-            ## Receive in order: throttle, steer, brake
-            t = float(ser.readline().decode('ASCII'))
-            s = float(ser.readline().decode('ASCII'))
-            b = float(ser.readline().decode('ASCII'))
-            
-            actor.apply_control(Carla.VehicleControl(throttle=t,steer=s,brake=b))
-
-            ''' 
-            Finish processing actuation commands here
-            
-            Here's how data is sent from Due:
-            - Throttle : float (-1 to 1)
-            - Steering : float (-1 to 1)
-            - Brakes   : float (0 for off 0.3 for on) <- because current implementation of brake
-                is siimply on/off.  Feel free to change on value of 0.3.
-            '''
-            
-            ### ACCESS/SEND LAT/LONG FROM LAST TICK ###
-            ### Can use for location if disabling GPS Sensor
-
-            #geo = world.get_map().transform_to_geolocation(logger.actorId.get_location())
-            #msg = geo.__str__() + '@'
-            #logger.setNmea(msg)
-            ###########
-            
-            ### ACCESS/SEND X/Y/Z FROM LAST TICK  #####
-            ### Can use for location if disabling GPS Sensor
-
-            #msg = logger.actorId.get_location().__str__() + '@'
-            #logger.setNmea(msg)
-            ########
-            
-            # Get the speed of Trike
-            getSpeed(logger.actorId, logger)
-            
-            # Send most current data
-            # ORDER MATTERS FOR ARDUINO CODE
-            logger.sendCyclometer(ser)
-            logger.sendNmea(ser)
-            
-            ### FOR SENSOR USAGE ###
-            logger.setListen()
-            #nmeaSensor.listen(lambda data: nmeaGPS.consoleout(data,logger))
-            #############
+    trike.demo()
 
 except KeyboardInterrupt:
-    print('\ndestroying %d sensors' % len(sensors))
-    for x in sensors:
-        Carla.command.DestroyActor(x)
-    print('\ndestroying vehicle')
-    actor.destroy()
+    trike.destroy()
     sys.exit()
-    
-'''
-Executes each time CARLA updates sensor data.  Obtains the speed of actor it is attached to.
-'''
 
-    
-if __name__ == '__main__':
 
-    try:
-        main()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        print('\ndone.')
