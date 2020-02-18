f79a0cc8ab013c025f50b0dbb545338abd6014c4
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2020-02-17 22:01:19 -0800

    Vehicle class expansion

--- Simulation/Elcano/vehicle.py
+++ Simulation/Elcano/vehicle.py
@@ -1,5 +1,6 @@
 import serial
 import Carla
+import math
 
 #Vehicle class, contans
 class Vehicle:
@@ -18,7 +19,7 @@ class Vehicle:
         #self.pulse
 
     #Initialize connection to simulator and spawn vehicle
-    def connectToSim(self, host = 'localhost', port = 2000, headless = True):
+    def connectToSim(self, host = 'localhost', port = 2000, headless = False):
 
         #Attempt to connect to the Carla server, timeout after 5 seconds
         self.client = Carla.Client(host, port)
@@ -37,10 +38,6 @@ class Vehicle:
         spawn = Carla.Transform(Carla.Location(x=15, y=20, z=5), Carla.Rotation(yaw=180))
         self.actor = self.world.spawn_actor(vehicle, spawn)
 
-        #Move server viewpoint to trike, will move this out of function with
-        #different options in UI for camera setup.
-        self.world.get_spectator().set_transform(self.actor.get_transform())
-
         if(headless):
             #Enable headless mode
             settings = self.world.get_settings()
@@ -49,89 +46,37 @@ class Vehicle:
 
         self.spawnSensors()
 
-    #Initialize connection to routerboard
-    def connectToRouter(self, port = 'COM4'):
-
-        #Set COM port for router communication
-        self.routerboard = serial.Serial(port, baudrate = 115200, timeout=5)
-
-        #Notify Due that system is starting
-        self.routerboard.write('f'.encode('utf-8')) 
-
     #For spawning all sensors onto vehicle, add sensors here.
     def spawnSensors(self):
 
         #Create and add all desired sensors
+        self.sensors.append(Camera(self.world, self.actor))
         self.sensors.append(GNSS(self.world, self.actor))
         self.sensors.append(IMU(self.world, self.actor))
+
 
     #For destroying the vehicle in simulator
     def destroy(self):
         for sensor in sensors:
             sensor.destroy()
         actor.destroy()
-
-    #Demo code, in the works.
-    def demo(self):
-
-        while True:
-            # Wait for ready queue from due
-            if self.routerboard.in_waiting:
-                
-                ''' 
-                ADD SERIAL READS FOR ACTUATION HERE 
-                
-                Currently just clears an arbitrary char in buffer sent from
-                router Due
-                '''
-
-                ## Receive in order: throttle, steer, brake
-                t = float(self.routerboard.readline().decode('ASCII'))
-                s = float(self.routerboard.readline().decode('ASCII'))
-                b = float(self.routerboard.readline().decode('ASCII'))
-                
-                actor.apply_control(Carla.VehicleControl(throttle=t,steer=s,brake=b))
-
-                ''' 
-                Finish processing actuation commands here
-                
-                Here's how data is sent from Due:
-                - Throttle : float (-1 to 1)
-                - Steering : float (-1 to 1)
-                - Brakes   : float (0 for off 0.3 for on) <- because current implementation of brake
-                    is siimply on/off.  Feel free to change on value of 0.3.
-                '''
-                
-                ### ACCESS/SEND LAT/LONG FROM LAST TICK ###
-                ### Can use for location if disabling GPS Sensor
-
-                #geo = world.get_map().transform_to_geolocation(logger.actorId.get_location())
-                #msg = geo.__str__() + '@'
-                #logger.setNmea(msg)
-                ###########
-                
-                
         
-    #Copied this function over will likely end up erased
-    def getSpeed(self, logger):
+    def getSpeed(self):
 
-        actor = self.actor
+        self.velocity = self.actor.get_velocity()
+        self.speed = math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y+velocity.z*velocity.z) 
 
-        circumference = .39 ##m
-        #actor = next((x for x in world.get_actors() if x.id == vehicles[vIndex][0]), None)
-        velocity = actor.get_velocity()
-        magnitude = math.sqrt(velocity.x*velocity.x + velocity.y*velocity.y+velocity.z*velocity.z) ##m/s
-        if(magnitude < 0.8333): ##random num between .5 and 1.5 when less than 3
-            magnitude = random.uniform(.139, .417)
-        else: ##add random error scaled by inverse of circumfrence
-            magnitude += random.uniform(-.5,.5)*(1/circumference)
+    def updateThrottle(self, t):
+        self.actor.apply_control(Carla.VehicleControl(throttle=t))
+
+    def updateSteering(self, s):
+        self.actor.apply_control(Carla.VehicleControl(steer=s))
+    
+    def updateBraking(self, b):
+        self.actor.apply_control(Carla.VehicleControl(brake=b))
+
+
         
-        #change m/s to s/pulse
-        wait = (1 / magnitude) * circumference
-        asString = "{:.3f}".format(wait) + '@'  #  The @ char is used to signify the end of a message
-        
-        logger.setCyclometer(asString)
-        #dueNative.write(asString.encode('utf-8'))
 
 ############ Will move these sensors into their own import ############
 #Each sensor will likely be sublasses of greater Sensor class
@@ -145,12 +90,21 @@ class GNSS:
         # Attach nmeaSensor to trike (speed directly taken from trike actor)
         blueprint = world.get_blueprint_library().find('sensor.other.gnss')
         transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
-        self.sensor = world.spawn_actor(blueprint,transform,actor)
+        self.sensor = world.spawn_actor(blueprint,transform,attach_to=actor)
+
+        #use built in listen function to help update camera, hacky fix for an issue
+        #with pinning camera to vehicle
+        self.sensor.listen(lambda data: self.updateCamera(world))
 
     def updateData(self):
         self.latitude = self.sensor.latitude
         self.longitude = self.sensor.longitude
         self.altitude = self.sensor.altitude
+
+    #hacky fix for keeping camera locked on car will figure something out better.
+    #possibly use pygame for a game hud and display
+    def updateCamera(self, world):
+        world.get_spectator().set_transform(self.sensor.get_transform())
 
     
         
@@ -173,4 +127,27 @@ class IMU:
         self.gyroscope = 0
         self.compass = 0
 
+
+class Camera:
+    def __init__(self, world, actor):
+
+        # Find the blueprint of the sensor.
+        blueprint = world.get_blueprint_library().find('sensor.camera.rgb')
+
+        # Modify the attributes of the blueprint to set image resolution and field of view.
+        blueprint.set_attribute('image_size_x', '720')
+        blueprint.set_attribute('image_size_y', '480')
+        blueprint.set_attribute('fov', '110')
+
+        # Set the time in seconds between sensor captures
+        blueprint.set_attribute('sensor_tick', '1.0')
+
+        # Provide the position of the sensor relative to the vehicle.
+        transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
+
+
+        # Tell the world to spawn the sensor, don't forget to attach it to your vehicle actor.
+        self.sensor = world.spawn_actor(blueprint, transform, attach_to=actor)
+
+        
     
