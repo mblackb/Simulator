07c067a3ebb1a1d1de3fa2a8a5d0d0b5434ea1ef
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2020-02-17 21:59:04 -0800

    Arduino Control of Simulator Testing

--- Simulation/megaTesting.ino
+++ Simulation/megaTesting.ino
@@ -1,4 +1,6 @@
-//For testing communication output from python interface
+//For testing serial communication between carla and an arduino
+//Using my own personal MEGA for these tests.
+//Shows various calls the arduino can make to the simulator
 
 
 void setup() {
@@ -6,13 +8,39 @@ void setup() {
     pinMode(13, OUTPUT);
 
     Serial.begin(115200);
-    //while (Serial.available() <= 0) {}; //wait for it to send a message before entering loop
-    
+    while(!Serial.available() ){}
 }
 
 void loop(){
+    
     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-    delay(1000);                       // wait for a second
+    updateBrake(0);  //Brakes: float (0 for off 0.3 for on)
+    updateThrottle(1); // Throttle : float (-1 to 1)
+    updateSteering(.5); //Steering : float (-1 to 1)
     digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-    delay(1000);                       // wait for a second
+    
+    delay(5000);
+
+    digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    updateThrottle(0);
+    updateBrake(0.3); //Brakes: float (0 for off 0.3 for on)
+    digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+
+    delay(5000);    
+
+}
+
+void updateThrottle(float value){
+    Serial.println(0); //mapped to throttle command in simulator
+    Serial.println(value);
+}
+
+void updateSteering(float value){
+    Serial.println(1); //mapped to steering command in simulator
+    Serial.println(value);
+}
+
+void updateBrake(float value){
+    Serial.println(2); //mapped to braking command in simulator
+    Serial.println(value);
 }
