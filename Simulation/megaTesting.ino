95e9ac9ed2942bf1f3c8e9e1f26ad5480fbe1df0
CarlaElcano <Carla.Elcano@outlook.com>
2020-02-19 14:13:43 -0800

    Minor changes to test arduino

--- Simulation/megaTesting.ino
+++ Simulation/megaTesting.ino
@@ -10,24 +10,28 @@ void setup() {
     pinMode(13, OUTPUT);
 
     Serial.begin(115200);
+    digitalWrite(13, HIGH);
     while(!Serial.available() ){}
+    digitalWrite(13, LOW);
 }
 
 void loop(){
 
+    //start driving, then turn right after 2 seconds, continue driving
     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-    updateBrake(0);  //Brakes: float (0 for off 0.3 for on)
     updateThrottle(1); // Throttle : float (-1 to 1)
+    delay(2000);
     updateSteering(.5); //Steering : float (-1 to 1)
     digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
     
+    //Wait 5 seconds and then stop throttle and apply brakes
     delay(5000);
-
     digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
     updateThrottle(0);
     updateBrake(0.3); //Brakes: float (0 for off 0.3 for on)
     digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
 
+    //Wait 5 seconds and start again
     delay(5000);    
 
 }
