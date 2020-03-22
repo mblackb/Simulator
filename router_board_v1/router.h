c52b0d36d98dfa612a04fc32a839836a27591116
LaunnieUW <61048934+LaunnieUW@users.noreply.github.com>
2020-03-21 22:37:12 -0700

    Receive from Carla Added
    Added functions for recieving data from Carla. Accelerometer and Magnetometer functions for sending to high level board over I2C are added. Currently using one I2C bus for each. Need to see if we can consolidate to one bus with multiple I2C addresses recognized so we can add gryroscope as well.

--- router_board_v1/router.h
+++ router_board_v1/router.h
@@ -1,15 +1,48 @@
+//#include "peripherals\L3GD20H-Gyro.h"
+//#include "peripherals\FGPMMOPA6H-GPS.h"
+//#include "peripherals\LSM303DLHC-Accel-Mag.h"
 
-#define BAUDRATE 115200
 
+// For UART connections
+#define BAUDRATE            115200
+#define RECEIVEBUFFERSIZE   10  // needs to be equal to or greater than highest number of receive bytes.
+
+// Define pins used for functions
+#define PULSE_PIN           5
+#define BRAKE_PIN           9
+#define STEER_PIN           6
+#define THROTTLE_PIN        A1
+
+// Accelerometer and Magnetometer
+#define ACCELADDRESS        0x19
+#define MAGADDRESS          0x1E
+#define ACCELINT            27
+#define MAGINT              28
+// interrupt macros
+#define SETACCELINTERRUPT   digitalWrite(ACCELINT,LOW);
+#define CLEARACCELINTERRUPT digitalWrite(ACCELINT,HIGH);
+#define SETMAGINTERRUPT     digitalWrite(MAGINT,LOW);
+#define CLEARMAGINTERRUPT   digitalWrite(MAGINT,HIGH);
 
 // Commands recognized by Carla - needs to match mapping on Carla.
 enum carlaCommand {
   throttleCommand = 0x00,
   steerCommand,
-  brakeCommand
+  brakeCommand,
+  accelCommand,
+  gyroCommand,
+  magCommand,
+  gpsCommand
 };
 
 // Define number of bytes to send for each command - needs to match number expected by Carla.
-#define THROTTLEBYTES 1
-#define BRAKEBYTES 1
-#define STEERBYTES 1
+#define THROTTLEBYTES       1
+#define BRAKEBYTES          1
+#define STEERBYTES          1
+#define ACCELBYTES          6
+#define GYROBYTES           1
+#define MAGBYTES            6
+#define GPSBYTES            2
+
+
+#define THROTTLENOISEMASK   5
--- router_board_v1/router_board_v1.ino
+++ router_board_v1/router_board_v1.ino
@@ -1,29 +1,20 @@
 /*
   Elcano Carla Simulator Capstone
-  UW Bothell, 2019
+  UW Bothell, 2020
   Advisor : Tyler Folsom
-  Team : Team: Zach Gale, Jonah Lim, Matthew Moscola, Francisco Navarro-Diaz
-  router_board_v1.ino
-  Version: 1.0
+  Team : Team: Launnie Ginn, Willeta Song, Colton Sellers, Brandon Thompson, Mariah Files
+  router_board.ino
+  Version: 2.0
   The main purposes of this program are:
     - Route actuation controls from low-level to Carla
     - Route simulated sensor data from Carla to low/high Level
-** Current implementation relies on order of sending to identify data
+** Data being sent to Carla is in the format:
+  {Header Byte}{Data Bytes}
+  The number of data bytes is determined by the type of data being sent, which is defined by the header byte.
 */
 
 #include "router.h"
-
-#define __arm__
-
-#define PULSE_PIN 5
-#define END_MARKER '@'  // char that signifies the end of a block of sensor data
-#define BRAKE_PIN 9
-#define STEER_PIN 6
-#define THROTTLE_PIN A1
-
-
-//SENSORS TO ADD: wheel angle sensor
-
+#include <Wire.h>
 
 // Download from github.com/ivanseidel/DueTimer
 //  -Follow instructions on github page to put in Arduino library
@@ -41,29 +32,35 @@ boolean newData = false;
 byte len = 0;
 int receivedData = 0;
 
-int loopStart = 0;
 
-//Steering variable setup
+//Steering variables
 volatile int pulseTime = 0;
 volatile long streerTimerStart = 0;
+byte steeringPrevious = 0;
 bool inSteering = false;
-//int steeringRead = 0;
-//int steerTime = 0;
-
-// These variables hold actuation data
-byte throttle = 0;
-byte brake = 0;
 byte steering = 0;
 
+
+// Throttle Variables
+volatile byte throttle = 0;
 byte throttleSent = 0;
-byte steeringPrevious = 0;
+
+// Braking Variables
+byte brake = 0;
 bool inBraking = false;
 bool brakeChange = false;
 
-//Set pins, prepare serial.
+byte accelDataBuffer[ACCELBYTES];
+byte magDataBuffer[MAGBYTES];
 
+
+
+//Set pins, prepare serial.
 void setup() {
+
+  // Serial port used for debug
   Serial.begin(BAUDRATE);
+  // Serial USB port used for Carla connection
   SerialUSB.begin(BAUDRATE);
 
   // Built in built in LED for debug
@@ -76,44 +73,37 @@ void setup() {
   // Start cyclometer pulse with arbitrarily large value
   Timer3.attachInterrupt(sendPulse).start(2000000);
 
-  // Initialize with no steering, brakes, or throttle
-  setActuation(throttle, steering, brake);
+  // Setup I2C for accelerometer and Magnetometer
+  // Currently using SDA/SCL for accelerometer and SDA1/SCL1
+  // For Magnetometer. Need to work out how to add
+  // Gyroscope address as a valid slave address.
+  Wire.begin(ACCELADDRESS);
+  Wire.onRequest(accelEvent);
+  Wire1.begin(MAGADDRESS);       // Accelerometer and magnetometer addresses
+  Wire1.onRequest(magEvent);  // register callback function
 
   // Wait until start msg from laptop
   while (SerialUSB.available() <= 0) {}
   SerialUSB.read(); // flush char in SerialUSB
   delay(100);   // Allow time for some data from Carla to be captured
-  loopStart = millis();
 
-  // Steering and braking handled in
+  // Steering and braking handled in interrupts
   attachInterrupt(STEER_PIN, manageSteering, CHANGE);
   attachInterrupt(BRAKE_PIN, manageBrake, CHANGE);
 }
 
 
+
+
 /*
-  Each loop does 3 things
-  - Reads actuation signals from low level board, updates last seen values.
-  - Sends formatted actuation data to PC connected to Carla.
-  - Retrieves simulated sensor data from the PC.
-
-
   TODO:
   -MOVE SENSOR READS TO INTERUPT
   -USE LOOP TO WRITE DATA TO CARLA
   -USE LOOP TO READ DATA TO CARLA
-  -MOVE CONVERSIONS TO PYTHON CODE
+
 */
 void loop() {
-  //int start = millis();
   receivedData = 0;
-
-  /////////////////////////////////////////////////
-  //  Update actuation instructions from low level
-  //  Throttle : from 50 to 225 (After reading signal about 40 to 140), analogRead from pin A1
-  //  Brake: on or off, digital pin 9
-  //  Steering: Pulse width, digital pin 6
-  //////////////////////////////////////////////////
 
   //We don't need to adjust the throttle if we are braking.
   if (!inBraking) {
@@ -122,17 +112,18 @@ void loop() {
     throttle = throttleRead >> 2;  // Read with 8 bit resolution
 
     //only send throttle data to Carla if it is different.
-    if (throttle > (throttleSent + 10) || throttle < (throttleSent - 10)) {
+    if (throttle > (throttleSent + THROTTLENOISEMASK) || throttle < (throttleSent - THROTTLENOISEMASK)) {
       throttleSent = throttle;
-      sendActuation(throttleCommand, &throttleSent, THROTTLEBYTES);
+      sendToCarla(throttleCommand, &throttleSent, THROTTLEBYTES);
       Serial.print("Throttle: ");
       Serial.println(throttleSent);
     }
   }
 
-  // If brake state has changed, send new brake value to Carla
+  // If brake state has changed, send new brake and throttle values to Carla
   if (brakeChange) {
-    sendActuation(brakeCommand, &brake, BRAKEBYTES);
+    sendToCarla(throttleCommand, &throttleSent, THROTTLEBYTES);
+    sendToCarla(brakeCommand, &brake, BRAKEBYTES);
     brakeChange = false;
     Serial.print("Brake: ");
     Serial.println(brake);
@@ -140,55 +131,64 @@ void loop() {
 
   // send steering data if it has been updated
   if (inSteering) {
-    sendActuation(steerCommand, &steering, STEERBYTES);
+    sendToCarla(steerCommand, &steering, STEERBYTES);
     inSteering = false;
     Serial.print("Steering: ");
     Serial.println(steering);
   }
 
+  if (SerialUSB.available()) {
+    volatile byte *receiveData = receiveFromCarla(1);
+    switch (receiveData[0]) {
+      case accelCommand:
+        // Get number of bytes associated with Accelerometer data
+        receiveData = receiveFromCarla(ACCELBYTES);
+        // Put received data into the accelerometer buffer
+        loadAccelBuffer(receiveData);
+        break;
+      case gyroCommand:
+        // Get number of bytes associated with gyroscope data
+        receiveData = receiveFromCarla(GYROBYTES);
+        // TODO: ADD FUNCTION FOR GYROSCOPE
+        break;
+      case magCommand:
+        // Get number of bytes associated with magnetometer data
+        receiveData = receiveFromCarla(MAGBYTES);
+        // Put received data into the magnetometer buffer
+        loadMagBuffer(receiveData);
+        break;
+      case gpsCommand:
+        // Get number of bytes associated with gps data
+        receiveData = receiveFromCarla(GPSBYTES);
+        // TODO: ADD FUNCTION FOR GPS
+        break;
+      default:
+        // if unknown code, flush serial bus data to ensure we aren't out of sync.
+        while (SerialUSB.available() > 0) {
+          receiveData = receiveFromCarla(1);
+        }
+        break;
+    }
+  }
 
 
 
-
-
-
-  ///////////////////////////////////////
-  // Send actuation control data to Carla
-  ///////////////////////////////////////
-
-  //  sendActuation();
-
-  ////////////////////////////////////////////
-  // Receive sensor data from Carla
-  // Process data based on order received
-  // Current order:
-  //  1st) Wait time in between cyclometer pulse in seconds
-  //  2nd) GPS data
-  /////////////////////////////////////////////
-  //**********************To DO****************************
-  //***************Update recieve packets from Carla*******
-  // Get time in between cyclometer pulse
-  // recvWithEndMarker();
-  // receiveNewData('s');
-
-  // Get GPS data
-  // recvWithEndMarker();
-  // receiveNewData('g');
-
-  // Gotta send it back
-
-  /////////////////////////////////////////////////////
-  // Finish data transactions
-  /////////////////////////////////////////////////////
-
-  // Make loop only happen once every 1/10th of a second
-  //while (millis() - start <= 100) {}
 
 }
 
-
-//Receives data from the programming port.
-void recvWithEndMarker() {
+volatile byte * receiveFromCarla(int byteCount) {
+  static volatile byte data[RECEIVEBUFFERSIZE];
+  for (byte i = 0; i < byteCount; i++) {
+    while (!SerialUSB.available()) {
+      //wait for data to be available
+    }
+    data[i] = SerialUSB.read();
+  }
+  return data;
+}
+/*
+  //Receives data from the programming port.
+  void recvWithEndMarker() {
   static byte ndx = 0;
   char rc;
 
@@ -210,10 +210,11 @@ void recvWithEndMarker() {
       newData = true;
     }
   }
-}
+  }
 
-//Does the appropriate action for each set of data retrieved
-void receiveNewData(char dataType) {
+
+  //Does the appropriate action for each set of data retrieved
+  void receiveNewData(char dataType) {
   if (newData == true) {
     // Identify type of data, process/route data appropriately
 
@@ -238,18 +239,12 @@ void receiveNewData(char dataType) {
     len = 0;
     receivedData++;
   }
-}
+  }
+*/
 
-
-//Allows you to set throttle, brake and steering in one call.
-void setActuation(byte t, byte s, byte b) {
-  throttle = t;
-  brake = b;
-  steering = s;
-}
-
-// Sends data to carla. Command list needs match command list for
-void sendActuation(carlaCommand mode, byte data[], int byteCount) {
+// Sends data to carla. Command list needs match command list on Carla side.
+// Defined in router.h as an enumeration
+void sendToCarla(carlaCommand mode, byte data[], int byteCount) {
   //SerialUSB.write(mode);
   char sendData[byteCount];
   for (byte i = 0; i < byteCount; i++) {
@@ -260,9 +255,40 @@ void sendActuation(carlaCommand mode, byte data[], int byteCount) {
   SerialUSB.write(sendData, byteCount);
 }
 
+// Load the Accelerometer buffer from the data received from Carla.
+void loadAccelBuffer(volatile byte receiveData[]) {
+  for (byte i = 0; i < ACCELBYTES; i++) {
+    accelDataBuffer[i] = receiveData[i];
+  }
+  // after buffer is loaded, inform high level data is ready
+  SETACCELINTERRUPT;
+}
 
-/*Sends pulse to low level for speed calculation.  Currently on random digital pin.  Has to be
-  implemented for corresponding anaolog pin.*/
+//Load the Magnetometer buffer from the data received from Carla.
+void loadMagBuffer(volatile byte receiveData[]) {
+  for (byte i = 0; i < MAGBYTES; i++) {
+    magDataBuffer[i] = receiveData[i];
+  }
+  // after buffer is loaded, inform high level data is ready
+  SETMAGINTERRUPT;
+}
+
+//Blink the on-board LED to test the cyclometer
+void Blink() {
+  digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
+}
+
+
+
+/****************************************************************
+
+                          ISR ROUTINES
+
+ ****************************************************************/
+
+/* THE CYCLOMETER FUNCTION NEEDS TO BE REDEFINED TO MATCH THE NEW SCOPE 
+ *  Sends pulse to low level for speed calculation. Currently on random digital pin.  Has to be
+ implemented for corresponding anaolog pin.*/
 
 void sendPulse() {
   //noInterrupts();
@@ -277,15 +303,10 @@ void sendPulse() {
 }
 
 
-//Blink the on-board LED to test the cyclometer
-void Blink() {
-  digitalWrite(LED_BUILTIN, digitalRead(LED_BUILTIN) ^ 1);
-}
-
-// Expected PWM frequency is 1KHz, so should be 1000 microseconds max
-// Steering ISR, is called by interrupt by steering signal
+// Measured PWM frequency from Low level board is ~490Hz, So PWM signal is between 0 and 2040 us.
+// Steering ISR, Interrupt on change.
 void manageSteering() {
-  //noInterrupts();
+
   //if the pin has gone HIGH, record the microseconds since the Arduino started up
   if (digitalRead(STEER_PIN) == HIGH)
   {
@@ -298,7 +319,7 @@ void manageSteering() {
     if (streerTimerStart != 0)
     {
       //record the pulse time
-      steeringPrevious = steering;
+      steeringPrevious = steering; // used to check if steering has changed.
       pulseTime = ((volatile long)micros() - streerTimerStart);
       steering = map(pulseTime, 0, 2040, -128, 127);
       if (steering != steeringPrevious)
@@ -308,17 +329,6 @@ void manageSteering() {
     }
   }
 
-
-
-  //steeringRead = pulseIn(STEER_PIN, HIGH);
-  //steeringRead = map(steeringRead, 1000, 1850, 0,18);
-
-  //if (steeringRead < 0) {
-  //  if (steeringRead < -9) steeringRead = -9;
-  //}
-
-  //steering = posdeci + steeringRead + nl;
-  //interrupts();
 }
 
 
@@ -326,7 +336,7 @@ void manageSteering() {
 void manageBrake () {
   if (digitalRead(BRAKE_PIN) == HIGH) {
     brake = 0xFF;  // Feel free to change
-    throttle = 0;
+    throttleSent = 0;
     inBraking = true;
   }
   else {
@@ -334,5 +344,15 @@ void manageBrake () {
     inBraking = false;
   }
   brakeChange = true;
-  //Serial.print(brake);
+}
+
+// ISR for read from Accelerometer address
+void accelEvent() {
+  Wire.write(accelDataBuffer, ACCELBYTES);
+  CLEARACCELINTERRUPT;
+}
+// ISR for read from Magnetometer address
+void magEvent() {
+  Wire1.write(magDataBuffer, MAGBYTES);
+  CLEARMAGINTERRUPT;
 }
