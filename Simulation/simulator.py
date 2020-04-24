2bfca4f2b8022fadcc79352e373f44c26dc52687
C-SELLERS <Colton.R.Sellers@gmail.com>
2020-04-24 15:51:05 -0700

    move to a main method for UI to call on

--- Simulation/simulator.py
+++ Simulation/simulator.py
@@ -32,53 +32,49 @@ import sys
 import Carla
 import Elcano
 
+def main():
+    #Wait for input before attempting to connect
+    print("Welcome to the Elcano Project Simulation")
+    #input("Press enter when prepared to connect to server")
 
-#Wait for input before attempting to connect
-print("Welcome to the Elcano Project Simulation")
-#input("Press enter when prepared to connect to server")
+    #Create the simulated vehicle and connect to server
+    trike = Elcano.SimulatedVehicle()
+    trike.connectToSim('localhost', 2000)
 
-#Create the simulated vehicle and connect to server
-trike = Elcano.SimulatedVehicle()
-trike.connectToSim('localhost', 2000)
-
-#Create the interface obj and connect to router
-Interface = Elcano.RouterboardInterface()
-Interface.connectToBoard('COM10', baudrate = 115200, timeout=5)
+    #Create the interface obj and connect to router
+    Interface = Elcano.RouterboardInterface()
+    Interface.connectToBoard('COM10', baudrate = 115200, timeout=5)
 
 
-#Create command map for incoming commands from routerboard
-commandMap = {
-    0:
-        Interface.actuateThrottle,
-    1:
-        Interface.actuateSteering,
-    2:
-        Interface.actuateBraking,
-    3:
-        Interface.getAccelerometer,
-}
+    #Create command map for incoming commands from routerboard
+    commandMap = {
+        0: Interface.actuateThrottle,
+        1: Interface.actuateSteering,
+        2: Interface.actuateBraking,
+        3: Interface.getAccelerometer,
+    }
 
-#After initial setup this is the loop that processes commands from routerboard
-try:
-    while True:
+    #After initial setup this is the loop that processes commands from routerboard
+    try:
+        while True:
 
-            #Commands in buffer
-            if Interface.serial.in_waiting:
+                #Commands in buffer
+                if Interface.serial.in_waiting:
 
-                #Get the command function, if it doesn't exist, return failure.
-                command = commandMap.get(list(Interface.serial.read())[0], "Failure")
+                    #Get the command function, if it doesn't exist, return failure.
+                    command = commandMap.get(list(Interface.serial.read())[0], "Failure")
 
-                #If its a failure, continue go back to while loop.
-                #Will add some logging later to catch these errors in communication.
-                if command == "Failure":
-                    continue
-                
-                #Execute the command
-                command(trike)
-                
+                    #If its a failure, continue go back to while loop.
+                    #Will add some logging later to catch these errors in communication.
+                    if command == "Failure":
+                        continue
+                    
+                    #Execute the command
+                    command(trike)
+                    
 
-except KeyboardInterrupt:
-    trike.destroy()
-    sys.exit()
+    except KeyboardInterrupt:
+        trike.destroy()
+        sys.exit()
 
 
