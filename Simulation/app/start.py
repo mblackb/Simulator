f4fd0d4df3d6ebfcab77643e8145d00b5443fdfb
Colton Sellers <41277998+C-SELLERS@users.noreply.github.com>
2020-04-30 02:02:59 -0700

    Working UI

--- Simulation/app/start.py
+++ Simulation/app/start.py
@@ -1,4 +1,5 @@
 import simulator
+import manual_control
 import serial.tools.list_ports
 import tkinter as tk
 from tkinter import ttk
@@ -12,33 +13,41 @@ class Application(tk.Frame):
 
     def createWidgets(self):
 
-        #Enter CARLA IP and PORT
-        #label
-        #Box 1 IP, Box 2 Port
+        #Box for IP of CARLA Server
+        tk.Label(self, text='Enter Server IP', bd=3).grid(row=7, column=0)
+        self.IPEntry = tk.StringVar()
+        IPBox = tk.Entry(self, textvariable=self.IPEntry)
+        IPBox.grid(row=7, column=1)
+        IPBox.insert(0,'localhost') #Default val
 
+        #Box for Port of server
+        tk.Label(self, text='Enter Server Port', bd=3).grid(row=8, column=0)
+        self.portEntry = tk.StringVar()
+        portBox = tk.Entry(self, textvariable=self.portEntry)
+        portBox.grid(row=8, column=1)
+        portBox.insert(0, '2000') #Default val
 
         #For Selecting Mode
         tk.Label(self, text='Select Mode', bd=3).grid(row=9, column=0)
         modes = ["Auto", "Manual"]
         self.modeSelected = tk.StringVar()
-
         box = ttk.Combobox(self, values=modes, justify="center", textvariable=self.modeSelected)
         box.grid(column=1, row=9, padx=(10), pady=(10))
         box.current(0)
 
         #Button for connecting elcano to CARLA
-        tk.Button(self, text="Connect to CARLA", bg="grey", fg="white", command = self.connectToCarla).grid(column=2, row=9, padx=(10), pady=(10))
+        tk.Button(self, text="Connect to CARLA", bg="grey", fg="white", command = self.connectToCarla).grid(column=1, row=10, padx=(10), pady=(10))
 
         #Button for quit
-        tk.Button(self, text='Quit', command=self.quit).grid(row=10, column=1, padx=(10), pady=(10)) 
+        tk.Button(self, text='Quit', command=self.quit).grid(row=10, column=0, padx=(10), pady=(10)) 
     
     def connectToCarla(self):
         if self.modeSelected.get() == 'Auto' :
-            #pop out for port selection
-            win = tk.Toplevel()
-            win.wm_title("COM Select")
+            #Popup window for selecting router port
+            popup = tk.Toplevel()
+            popup.wm_title("COM Select")
 
-            tk.Label(win, text='Select COM Port of Routerboard', bd=3).grid(row=0, column=0)
+            tk.Label(popup, text='Select COM Port of Routerboard', bd=3).grid(row=0, column=0)
 
             #Get COM Devices and add them to a dictionary
             devices =  list(serial.tools.list_ports.comports())
@@ -48,23 +57,23 @@ class Application(tk.Frame):
 
             #Setup Combobox of Devices
             self.portSelected = tk.StringVar()
-            box = ttk.Combobox(win, values=list(self.deviceDict.keys()), justify="center", textvariable=self.portSelected, width=30)
+            box = ttk.Combobox(popup, values=list(self.deviceDict.keys()), justify="center", textvariable=self.portSelected, width=30)
             box.grid(column=0, row=1, padx=(10), pady=(10))
             box.current(0)
 
             #Button for go
-            tk.Button(win, text="GO", bg="grey", fg="white", command = self.startSim).grid(column=0, row=2, padx=(10), pady=(10))
+            tk.Button(popup, text="GO", bg="grey", fg="white", command = self.startElcanoSim).grid(column=0, row=2, padx=(10), pady=(10))
 
 
             
         else :
-            print("This will start the manual control of carla, use python examples code from carla")
+            manual_control.main() #Need to add passing of ip and port
 
 
-    def startSim(self):
+    def startElcanoSim(self):
         COMPort = self.deviceDict[self.portSelected.get()]
-        simulator.main(COMPort)
-        
+        simulator.main(COMPort, self.IPEntry, self.portEntry)
+    
 
 
 app = Application()                       
