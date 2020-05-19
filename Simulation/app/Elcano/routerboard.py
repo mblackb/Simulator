3115d0093fd48db66adbb90fdc2139edb7f87fe2
C-SELLERS <Colton.R.Sellers@gmail.com>
2020-05-18 19:51:55 -0700

    A ton of changes, but this code is not yet ready

--- Simulation/app/Elcano/routerboard.py
+++ Simulation/app/Elcano/routerboard.py
@@ -2,21 +2,76 @@ import serial
 import time
 
 class RouterboardInterface:
+    """
+    Acts as the interface the routerboard uses to communicate to Carla.
 
-    #Constructor, requires an obj for simulate
-    def __init__(self):
-        self.name = "router"
+    Reads in commands from buffer and executes them, also sends data back to router.
+
+    Accepted params:
+    COMPort - the COM port the routerboard is plugged into.
+    """
+
+    def __init__(self, COMPort, simVehicle):
+        self.name = "routerboard"
+        self.COMPort = COMPort
+        self.simVehicle = simVehicle
+
+        #Create command map for incoming commands from routerboard
+        self.commandMap = {
+            0: self.actuateThrottle,
+            1: self.actuateSteering,
+            2: self.actuateBraking,
+            3: self.getAccelerometer,
+        }
+
+        self.connectToBoard(COMPort, baudrate = 115200, timeout=5)
 
 
-    #Connect to the router board
     def connectToBoard(self, COMport, baudrate, timeout):
+        """
+        Actually connects to the router board via serial.
+
+        Accepted params:
+        COMPort - the COM port the routerboard is plugged into.
+        baudrate -
+        timeout - Max time it will wait to establish communication over serial.
+        """
+
         self.serial = serial.Serial(port=COMport, baudrate=baudrate, timeout=timeout)
         time.sleep(1)
         self.serial.write('f'.encode('utf-8')) 
 
+
+    def controlLoop(self):
+        """
+        Enters the control loop which will handle the commands and sending of data
+        """
+
+        #If there is a command waiting
+        if self.serial.in_waiting:
+
+            #Get the command function, if it doesn't exist, will return None
+            command = self.commandMap.get(list(self.serial.read())[0])
+
+            #If its none continue the loop looking for commands
+            #Will add some logging later to catch these errors in communication.
+            if command == None:
+                return
+            
+            #Execute the command
+            command()
+
+        
+        #Send new data across, maybe limit to once a second?
+        self.getGPS()
+
     
-    #Command from router to change throttle
-    def actuateThrottle(self, simVehicle) :
+    
+    def actuateThrottle(self) :
+        """
+        Command to update throttle on vehicle.
+        Reads in the next byte from serial to determine this value.
+        """
 
         #Get the argument from the router board and convert into a Carla value
         #Value comes in as a 8 bit unsigned int (0 to 255)
@@ -25,11 +80,15 @@ class RouterboardInterface:
 
         #Throttle : float (-1 to 1) 
         #No reverse option for now
-        simVehicle.updateThrottle(arg)
+        self.simVehicle.updateThrottle(arg)
 
 
-    #Command from router to change steering
-    def actuateSteering(self, simVehicle) :
+
+    def actuateSteering(self) :
+        """
+        Command to update steering on vehicle.
+        Reads in the next byte from serial to determine this value.
+        """
 
         #Get the argument from the router board and convert into a Carla value
         #Value comes in as a 8 bit signed int (-128 to 127)
@@ -37,11 +96,14 @@ class RouterboardInterface:
         carlaValue = mapValue(arg, 0, 255, -1, 1)
 
         #Steering : flaot (-1 to 1)
-        simVehicle.updateSteering(carlaValue)
+        self.simVehicle.updateSteering(carlaValue)
 
 
-    #Command from router to change braking
-    def actuateBraking(self, simVehicle) :
+    def actuateBraking(self) :
+        """
+        Command to update braking on vehicle.
+        Reads in the next byte from serial to determine this value.
+        """
 
         #Get the argument from the router board and convert into a Carla value
         #Value comes in as a 8 bit unsigned int (0 to 255)
@@ -49,35 +111,36 @@ class RouterboardInterface:
         carlaValue = mapValue(arg, 0, 255, 0, 1)
 
         #Braking : float (0 to 1)
-        simVehicle.updateBraking(carlaValue)
+        self.simVehicle.updateBraking(carlaValue)
     
 
-    def getAccelerometer (self, simVehicle):
+    def getAccelerometer (self):
 
         #EXAMPLE for sending data back to router, doesn't actually
         #do anything yet
-        self.serial.write(simVehicle.Accelerometer)
 
-    def getGPS(self, simVehicle):
+        IMU = self.simVehicle.IMUSensor
 
-        lat = simVehicle.GPS.latitude
-        lon = simVehicle.GPS.longitude
+
+    def getGPS(self):
+        """
+        Function to send gps data back to router board.
+        Collects data from vehicle sensor and writes it to serial in the correct format.
+        """
+
+        lat = self.simVehicle.GNSSSensor.latitude
+        lon = self.simVehicle.GNSSSensor.longitude
 
         #write the messages, need to adapt format.
-        self.serial.write(6)
-        self.serial.write(lat)
-        self.serial.write(lon)
+        #self.serial.write(6)
+        #self.serial.write(lat)
+        #self.serial.write(lon)
 
         ##Header byte is 6 for GPS, reference router.h
         ##LAT, LONG, ALT, HH:MM:SS.sss once a second, maybe a thread?
         ##lat[9],latdir,long[10],longdir (ddmm.mmmmN/Sdddmm.mmmmE/W) i.e: 4559.4810N12269.3800W
         ##dd is degrees mm.mmmm is minutes ss is seconds
 
-
-
-        
-
-    
 
 def mapValue(value, leftMin, leftMax, rightMin, rightMax):
     # Figure out how 'wide' each range is
--- Simulation/app/Elcano/simulatedvehicle.py
+++ Simulation/app/Elcano/simulatedvehicle.py
@@ -1,6 +1,10 @@
 import math
 import Carla
+from Carla import ColorConverter as cc
 import weakref
+import pygame
+import numpy as np
+
 
 class SimulatedVehicle:
     """
@@ -10,7 +14,9 @@ class SimulatedVehicle:
     """
 
     #Constructor for vehicle class
-    def __init__(self):
+    def __init__(self, world, hud):
+
+        self.world = world
 
         #Variables related to simulated vehicle
         self.sensors = []
@@ -19,24 +25,8 @@ class SimulatedVehicle:
         self.throttle = 0
         self.steering = 0
         self.braking = 0
+        self.hud = hud
 
-    #Initialize connection to simulator and spawn vehicle
-    def connectToSim(self, host = 'localhost', port = 2000, headless = False):
-        """
-        Connects the trike to the simulator server
-
-        Accepted params:
-        host - string for server location, default localhost
-        port - int for port of server, default 2000
-        headless - boolean for a headless server, default False
-        """
-        
-        #Attempt to connect to the Carla server, timeout after 5 seconds
-        self.client = Carla.Client(host, port)
-        self.client.set_timeout(5.0)
-
-        #Get the world from the server
-        self.world = self.client.get_world()
 
         #Load Carlas blueprint library
         self.blueprint_library = self.world.get_blueprint_library()
@@ -48,28 +38,28 @@ class SimulatedVehicle:
         spawn = Carla.Transform(Carla.Location(x=34, y=7, z=1), Carla.Rotation(yaw=0))
         self.actor = self.world.spawn_actor(vehicle, spawn)
 
-        if(headless):
-            #Enable headless mode
-            settings = self.world.get_settings()
-            settings.no_rendering_mode = True
-            self.world.apply_settings(settings)
-
         #spawn the sensors for the vehicle
         self.spawnSensors()
 
     #For spawning all sensors onto vehicle, add sensors here.
     def spawnSensors(self):
 
-        #Create and add all desired sensors
-        self.sensors.append(Camera(self.world, self.actor))
-        self.sensors.append(GNSS(self.world, self.actor))
-        self.sensors.append(IMU(self.world, self.actor))
+        # Set up the sensors.
+        self.GNSSSensor = GNSSSensor(self.world, self.actor)
+        self.IMUSensor = IMUSensor(self.world, self.actor)
+        self.camera_manager = CameraManager(self.actor, self.world, self.hud, 2.2)
+        self.camera_manager.transform_index = 0
+        self.camera_manager.set_sensor(0, notify=False)
+
 
 
     #For destroying the vehicle in simulator
     def destroy(self):
-        for sensor in self.sensors:
-            sensor.sensor.destroy()
+    
+        self.GNSSSensor.sensor.destroy()
+        self.IMUSensor.sensor.destroy()
+        self.camera_manager.sensor.destroy()
+
         self.actor.destroy()
         
     def getSpeed(self):
@@ -97,19 +87,21 @@ class SimulatedVehicle:
         self.actor.apply_control(Carla.VehicleControl(throttle=self.throttle,steer=self.steering,brake=self.braking))
 
 
-        
+################################# SENSORS  #####################################
 
-############ Will move these sensors into their own import ############
-#Each sensor will likely be sublasses of greater Sensor class
-#Will be able to determine values from carla and send them to router
-#
-#Need to move sensor function from previous implementation to here.
+# ==============================================================================
+# -- GNSSSensor ----------------------------------------------------------------
+# ==============================================================================
 
-class GNSS:
+class GNSSSensor:
+    """
+    Sensor class for collecting GNSS data from Carla
+    """
+
     def __init__(self, world, actor):
 
+
         # Init default values
-        self.sensor = None
         self.latitude = 0
         self.longitude = 0
 
@@ -120,59 +112,194 @@ class GNSS:
 
         #To avoid circular ref we are going to use a weak reference.
         weak_self = weakref.ref(self)
-        self.sensor.listen(lambda event: GnssSensor._on_gnss_event(weak_self, event))
+        self.sensor.listen(lambda event: GNSSSensor._on_gnss_event(weak_self, event))
 
     #Storing the gps info on every update
     @staticmethod
     def _on_gnss_event(weak_self, event):
+        """
+        To store the GNSS data every time it changes in CARLA
+        """
+
         self = weak_self()
         if not self:
             return
+
         self.latitude = event.latitude
         self.longitude = event.longitude
         self.altitude = event.altitude
     
         
-class IMU:
+# ==============================================================================
+# -- IMUSensor -----------------------------------------------------------------
+# ==============================================================================
+
+class IMUSensor(object):
+    """
+    Sensor class for collecting IMU data from Carla
+    """
+
     def __init__(self, world, actor):
 
-        #Get sensor and spawn it onto the vehicle
-        blueprint = world.get_blueprint_library().find('sensor.other.imu')
-        transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
-        self.sensor = world.spawn_actor(blueprint, transform, actor)
+        #Default values
+        self.accelerometer = (0.0, 0.0, 0.0)
+        self.gyroscope = (0.0, 0.0, 0.0)
+        self.compass = 0.0
 
-        self.name = IMU
-
-    def updateData(self):
-        #Do the thing with the data Brandon.
-        #You got this, change values to whatever structure you need for it
-
-        self.accelerometer = 0
-        self.gyroscope = 0
-        self.compass = 0
+        # Attach the sensor to the vehicle
+        bp = world.get_blueprint_library().find('sensor.other.imu')
+        self.sensor = world.spawn_actor(bp, Carla.Transform(), attach_to=actor)
 
 
-class Camera:
-    def __init__(self, world, actor):
+        # We need to pass the lambda a weak reference to self to avoid circular
+        # reference.
+        weak_self = weakref.ref(self)
+        self.sensor.listen(lambda sensor_data: IMUSensor._IMU_callback(weak_self, sensor_data))
 
-        # Find the blueprint of the sensor.
-        blueprint = world.get_blueprint_library().find('sensor.camera.rgb')
+    @staticmethod
+    def _IMU_callback(weak_self, sensor_data):
+        """
+        To store the IMU data every time it changes in CARLA
+        """
 
-        # Modify the attributes of the blueprint to set image resolution and field of view.
-        blueprint.set_attribute('image_size_x', '720')
-        blueprint.set_attribute('image_size_y', '480')
-        blueprint.set_attribute('fov', '110')
+        self = weak_self()
+        if not self:
+            return
 
-        # Set the time in seconds between sensor captures
-        blueprint.set_attribute('sensor_tick', '1.0')
+        limits = (-99.9, 99.9)
+        self.accelerometer = (
+            max(limits[0], min(limits[1], sensor_data.accelerometer.x)),
+            max(limits[0], min(limits[1], sensor_data.accelerometer.y)),
+            max(limits[0], min(limits[1], sensor_data.accelerometer.z)))
+        self.gyroscope = (
+            max(limits[0], min(limits[1], math.degrees(sensor_data.gyroscope.x))),
+            max(limits[0], min(limits[1], math.degrees(sensor_data.gyroscope.y))),
+            max(limits[0], min(limits[1], math.degrees(sensor_data.gyroscope.z))))
+        self.compass = math.degrees(sensor_data.compass)
 
-        # Provide the position of the sensor relative to the vehicle.
-        transform = Carla.Transform(Carla.Location(x=0.8, z=1.7))
+
+# ==============================================================================
+# -- CameraManager -------------------------------------------------------------
+# ==============================================================================
 
 
-        # Tell the world to spawn the sensor, don't forget to attach it to your vehicle actor.
-        self.sensor = world.spawn_actor(blueprint, transform, attach_to=actor)
+class CameraManager(object):
+    """
+    Sensor class for collecting camera data from Carla
 
+    Completely taken from manualcontrol.py so it is not entirely understood by me.
+    """
 
-        
-    
+    def __init__(self, actor, world, hud, gamma_correction):
+        self.sensor = None
+        self.surface = None
+        self.actor = actor
+        self.hud = hud
+        self.recording = False
+
+        bound_y = 0.5 + self.actor.bounding_box.extent.y
+        Attachment = Carla.AttachmentType
+
+        self._camera_transforms = [
+            (Carla.Transform(Carla.Location(x=-5.5, z=2.5), Carla.Rotation(pitch=8.0)), Attachment.SpringArm),
+            (Carla.Transform(Carla.Location(x=1.6, z=1.7)), Attachment.Rigid),
+            (Carla.Transform(Carla.Location(x=5.5, y=1.5, z=1.5)), Attachment.SpringArm),
+            (Carla.Transform(Carla.Location(x=-8.0, z=6.0), Carla.Rotation(pitch=6.0)), Attachment.SpringArm),
+            (Carla.Transform(Carla.Location(x=-1, y=-bound_y, z=0.5)), Attachment.Rigid)]
+        self.transform_index = 1
+        self.sensors = [
+            ['sensor.camera.rgb', cc.Raw, 'Camera RGB', {}],
+            ['sensor.camera.depth', cc.Raw, 'Camera Depth (Raw)', {}],
+            ['sensor.camera.depth', cc.Depth, 'Camera Depth (Gray Scale)', {}],
+            ['sensor.camera.depth', cc.LogarithmicDepth, 'Camera Depth (Logarithmic Gray Scale)', {}],
+            ['sensor.camera.semantic_segmentation', cc.Raw, 'Camera Semantic Segmentation (Raw)', {}],
+            ['sensor.camera.semantic_segmentation', cc.CityScapesPalette,
+                'Camera Semantic Segmentation (CityScapes Palette)', {}],
+            ['sensor.lidar.ray_cast', None, 'Lidar (Ray-Cast)', {}],
+            ['sensor.camera.rgb', cc.Raw, 'Camera RGB Distorted',
+                {'lens_circle_multiplier': '3.0',
+                'lens_circle_falloff': '3.0',
+                'chromatic_aberration_intensity': '0.5',
+                'chromatic_aberration_offset': '0'}]]
+        bp_library = world.get_blueprint_library()
+
+        for item in self.sensors:
+            bp = bp_library.find(item[0])
+            if item[0].startswith('sensor.camera'):
+                bp.set_attribute('image_size_x', str(hud.dim[0]))
+                bp.set_attribute('image_size_y', str(hud.dim[1]))
+                if bp.has_attribute('gamma'):
+                    bp.set_attribute('gamma', str(gamma_correction))
+                for attr_name, attr_value in item[3].items():
+                    bp.set_attribute(attr_name, attr_value)
+            elif item[0].startswith('sensor.lidar'):
+                bp.set_attribute('range', '50')
+            item.append(bp)
+
+        self.index = None
+
+    def toggle_camera(self):
+        self.transform_index = (self.transform_index + 1) % len(self._camera_transforms)
+        self.set_sensor(self.index, notify=False, force_respawn=True)
+
+    def set_sensor(self, index, notify=True, force_respawn=False):
+        index = index % len(self.sensors)
+        needs_respawn = True if self.index is None else \
+            (force_respawn or (self.sensors[index][2] != self.sensors[self.index][2]))
+        if needs_respawn:
+            if self.sensor is not None:
+                self.sensor.destroy()
+                self.surface = None
+            self.sensor = self.actor.get_world().spawn_actor(
+                self.sensors[index][-1],
+                self._camera_transforms[self.transform_index][0],
+                attach_to=self.actor,
+                attachment_type=self._camera_transforms[self.transform_index][1])
+
+            # We need to pass the lambda a weak reference to self to avoid
+            # circular reference.
+            weak_self = weakref.ref(self)
+            self.sensor.listen(lambda image: CameraManager._parse_image(weak_self, image))
+        if notify:
+            self.hud.notification(self.sensors[index][2])
+        self.index = index
+
+    def next_sensor(self):
+        self.set_sensor(self.index + 1)
+
+    def toggle_recording(self):
+        self.recording = not self.recording
+        self.hud.notification('Recording %s' % ('On' if self.recording else 'Off'))
+
+    def render(self, display):
+        if self.surface is not None:
+            display.blit(self.surface, (0, 0))
+
+    @staticmethod
+    def _parse_image(weak_self, image):
+        self = weak_self()
+        if not self:
+            return
+        if self.sensors[self.index][0].startswith('sensor.lidar'):
+            points = np.frombuffer(image.raw_data, dtype=np.dtype('f4'))
+            points = np.reshape(points, (int(points.shape[0] / 3), 3))
+            lidar_data = np.array(points[:, :2])
+            lidar_data *= min(self.hud.dim) / 100.0
+            lidar_data += (0.5 * self.hud.dim[0], 0.5 * self.hud.dim[1])
+            lidar_data = np.fabs(lidar_data)  # pylint: disable=E1111
+            lidar_data = lidar_data.astype(np.int32)
+            lidar_data = np.reshape(lidar_data, (-1, 2))
+            lidar_img_size = (self.hud.dim[0], self.hud.dim[1], 3)
+            lidar_img = np.zeros((lidar_img_size), dtype = int)
+            lidar_img[tuple(lidar_data.T)] = (255, 255, 255)
+            self.surface = pygame.surfarray.make_surface(lidar_img)
+        else:
+            image.convert(self.sensors[self.index][1])
+            array = np.frombuffer(image.raw_data, dtype=np.dtype("uint8"))
+            array = np.reshape(array, (image.height, image.width, 4))
+            array = array[:, :, :3]
+            array = array[:, :, ::-1]
+            self.surface = pygame.surfarray.make_surface(array.swapaxes(0, 1))
+        if self.recording:
+            image.save_to_disk('_out/%08d' % image.frame)
+
